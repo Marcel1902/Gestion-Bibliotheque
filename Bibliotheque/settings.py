e316b339f72eb47e72496d3d136787af822dc68f
Marcel <faniloniainatanguymarcel@gmail.com>
2023-12-27 10:07:12 +0300

    changement du base des données sqlite en mysql

--- Bibliotheque/settings.py
+++ Bibliotheque/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'Bibliotheque.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Bibliotheque',
+        'USER': 'root',
+        'PASSWORD': '19102002Fai.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
